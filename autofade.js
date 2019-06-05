b51064a85e6237993f0eb81354875ce6e4718d99
IJ <v77bell@gmail.com>
2019-06-04 23:03:21 -0400

    Update autofade.js

--- autofade.js
+++ autofade.js
@@ -8,10 +8,10 @@
 // If there are *2* markers, the first marker is used for end point of the
 // fade in, and the second marker is used to define the start of the fade out.
 
-transition = 20;    // transition time in frames
+transition = 20;
 
 if ( marker.numKeys < 2 ) {
-    tSecs = transition / ( 1 / thisComp.frameDuration ); // convert to seconds
+    tSecs = transition / ( 1 / thisComp.frameDuration );
     linear( time, inPoint, inPoint + tSecs, 0, 100 ) - linear( time, outPoint - tSecs, outPoint, 0, 100 )
 } else {
     linear( time, inPoint, marker.key(1).time, 0, 100 ) - linear( time, marker.key(2).time, outPoint, 0, 100 )
